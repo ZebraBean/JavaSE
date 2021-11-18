464fd8be735b2941599e959b1784c2503623b885
Telesporter <51327163+Telesporter@users.noreply.github.com>
2021-11-18 20:25:14 +0800

    Add files via upload

--- JavaSE16/test.java
+++ JavaSE16/test.java
@@ -3,15 +3,7 @@ package Tests.JavaSE16;
 import java.awt.*;
 
 public class test {
-    public static void main(String[] args) {
-        Shape shape1 = new Flower();
-        Shape shape2 = new Cycle();
-        Shape shape3 = new Rect();
-        drawShape(shape1);
-        drawShape(shape2);
-        drawShape(shape3);
-    }
-    public static void drawShape(Shape shape){
-        shape.draw();
-    }
+
 }
+
+
