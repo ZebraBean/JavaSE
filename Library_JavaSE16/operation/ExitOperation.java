21e7542bd4ecdd7aa8fa763d50d6a8c0e622bc3d
Telesporter <51327163+Telesporter@users.noreply.github.com>
2021-11-23 15:57:20 +0800

    Add files via upload

---
+++ Library_JavaSE16/operation/ExitOperation.java
@@ -0,0 +1,17 @@
+package Library_JavaSE16.operation;
+
+import Library_JavaSE16.book.BookList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 13498
+ * Date: 2021/11/22
+ * Time: 14:04
+ * Description: 退出系统
+ */
+public class ExitOperation implements IOperation {
+    public void work(BookList bookList){
+        System.out.println("退出系统");
+        System.exit(0);  //退出系统
+    }
+}
---
+++ Library_JavaSE16/operation/IOperation.java
@@ -0,0 +1,14 @@
+package Library_JavaSE16.operation;
+
+import Library_JavaSE16.book.BookList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 13498
+ * Date: 2021/11/22
+ * Time: 14:06
+ * Description: No Description
+ */
+public interface IOperation {
+    void work(BookList bookList);
+}
