6fd24546241dad1e18fd1d65f64067659eb45069
Telesporter <51327163+Telesporter@users.noreply.github.com>
2021-11-24 20:59:01 +0800

    Add files via upload

---
+++ JavaSE16/Test.java
@@ -0,0 +1,18 @@
+package Tests.JavaSE16;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 13498
+ * Date: 2021/11/24
+ * Time: 17:50
+ * Description: No Description
+ */
+public class Test {
+    public static void main(Test[] args) {
+        String str1 = "hello";
+        String str2 = new String("hello");
+        char[] value = {'a','b','c','d'};
+        String str3 = new String(value);
+
+    }
+}
